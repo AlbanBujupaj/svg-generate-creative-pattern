f42519cde05fa4ae4e5d0ca83050ecc25bdc76dc
Alban Bujupaj <abujupaj@gmail.com>
2025-07-04 04:07:40 -0400

    types declarations file added

---
+++ src/declarations.d.ts
@@ -0,0 +1,11 @@
+declare module '@georgedoescode/generative-utils' {
+  export function map(
+    value: number,
+    inMin: number,
+    inMax: number,
+    outMin: number,
+    outMax: number
+  ): number;
+
+  // Add more function declarations here if needed
+}
